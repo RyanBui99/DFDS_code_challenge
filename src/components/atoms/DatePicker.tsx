84aab41bd186f0042febade2eba0849f21f61b71
RyanBui99 <buiryan99@gmail.com>
2024-06-12 00:09:52 +0200

    Update datePicker component to use timePicker as well and change constant to show min instead of sec

--- src/components/atoms/DatePicker.tsx
+++ src/components/atoms/DatePicker.tsx
@@ -8,13 +8,18 @@ import { cn } from "~/utils";
 import { Calendar } from "../ui/calendar";
 import { CalendarIcon } from "lucide-react";
 import { FunctionComponent } from "react";
+import { TimePicker } from ".";
+import { Label } from "../ui/label";
 
 interface DatePickerProps {
-  setDate: (date: Date) => void;
+  setDate: (date: Date | undefined) => void;
   date: Date;
 }
 
 const DatePicker: FunctionComponent<DatePickerProps> = ({ setDate, date }) => {
+  const minuteRef = React.useRef<HTMLInputElement>(null);
+  const hourRef = React.useRef<HTMLInputElement>(null);
+
   return (
     <Popover>
       <PopoverTrigger asChild>
@@ -26,7 +31,7 @@ const DatePicker: FunctionComponent<DatePickerProps> = ({ setDate, date }) => {
           )}
         >
           <CalendarIcon className="mr-2 h-4 w-4" />
-          {date ? format(date, "PPP") : <span>Pick a date</span>}
+          {date ? format(date, "PPP HH:mm") : <span>Pick a date</span>}
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-auto p-0">
@@ -36,6 +41,28 @@ const DatePicker: FunctionComponent<DatePickerProps> = ({ setDate, date }) => {
           onSelect={(date) => date && setDate(date)}
           initialFocus
         />
+
+        <div className="flex justify-center gap-7 pb-4">
+          <div className="grid gap-1 ">
+            <Label htmlFor="hours">Hours</Label>
+            <TimePicker
+              picker="hours"
+              date={date}
+              setDate={setDate}
+              ref={hourRef}
+            />
+          </div>
+
+          <div className="grid gap-1 ">
+            <Label htmlFor="minutes">Minutes</Label>
+            <TimePicker
+              picker="minutes"
+              date={date}
+              setDate={setDate}
+              ref={minuteRef}
+            />
+          </div>
+        </div>
       </PopoverContent>
     </Popover>
   );
--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,2 @@
-export const TABLE_DATE_FORMAT = "dd/MM/yyyy HH:ss";
+// export const TABLE_DATE_FORMAT = "dd/MM/yyyy HH:ss";
+export const TABLE_DATE_FORMAT = "dd/MM/yyyy HH:mm";
